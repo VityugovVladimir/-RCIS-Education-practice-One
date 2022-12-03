2792b5de8a793ac3f8f5e6aa918f0a5b6f3668fd
Витюгов Владимир Евгеньевич <gr612_vivev@ttit.local>
2022-12-03 11:02:42 +0700

    First commit. Create console app

--- rcis_ep_task_one6/bin/rcis_ep_task_one6.dart
+++ rcis_ep_task_one6/bin/rcis_ep_task_one6.dart
@@ -64,42 +64,39 @@ import 'dart:math';
 
 
 // zadanie 5
-void main() {
-  List <int> nums = [];
-  int sum = 0;
-  while( 1!= 0){
-    try{
-      stdout.write('Ввeдите число: ');
-      int a = int.parse(stdin.readLineSync()!);
-      if(a > 0){
-        break;
-      }
-    } 
-    catch(e){
-      print('данные неверные');
-      continue;
-    }
-  } 
-  while(1 > 0){
-    try{
-      stdout.write('Ввeдите число: ');
-      int num = int.parse(stdin.readLineSync()!);
-      if (num < 0){
-        print('Вы ввели отрицательное число');
-        break;
-      }
-      else{
-        nums.add(num);
-      }
-    }
-    catch(e){
-      print('данные не верны');
-    }
-  }
-  for(int i = 0; i< nums.length; i++){
-    if(nums[i] % 2 == 0){
-      sum+= nums[i];
-    }
-  }
-  print('сумма делящихся положительных чисел = $sum');
-}
+// void main()  {
+//   stdout.write('введите n: ');
+//   int n = 0;
+//   int m = 0;
+//   try{
+//    n = int.parse(stdin.readLineSync()!);
+//   }catch(e){
+//     print('вы ввели не число!');
+//   }
+//   stdout.write('введите m: ');
+//    try{
+//    m = int.parse(stdin.readLineSync()!);
+//   }catch(e){
+//     print('вы ввели не число!');
+//   }
+//   List<List<int>> nums = List.generate(n, (index) => List.generate(m + 1, ((index) => Random().nextInt(2))));
+//   for(int i = 0; i < n; i++){
+//     int count = 0;
+//     for(int j = 0; j < m; j++){
+//       if(nums[i][j] == 1){
+//         count++;
+//       }
+//     }
+//     if(count % 2 != 0){
+//       nums[i][m] = 1;
+//     }else{
+//       nums[i][m] = 0;
+//     }
+//   }
+//   for(int i = 0; i < n; i++){
+//     for(int j = 0; j < m + 1; j++){
+//       stdout.write('${nums[i][j]} ');
+//     }
+//     print('');
+//   }
+// }
