aec099e5fe5b5bac064f78e6db4cf49ca1d76d9f
Витюгов Владимир Евгеньевич <gr612_vivev@ttit.local>
2022-12-03 10:14:15 +0700

    First commit. Create console app

--- rcis_ep_task_one6/bin/rcis_ep_task_one6.dart
+++ rcis_ep_task_one6/bin/rcis_ep_task_one6.dart
@@ -7,90 +7,33 @@ import 'dart:math';
 // Практическая 1.6
 
 
+// zadanie 1
+
+// void main() {
+// List <String> words = [];
+// List <String> input = File ("numsTask1.txt").readAsLinesSync();
+// for (var nums in input) {
+// input = nums.split(' ');
+// words += input;
+// }
+// for (int i = 0 ; i < words.length; i++){
+// if (words[i].length % 2 != 0) {
+// stdout.write ("${words[i]} ");
+// }
+// }
+// }
+
+
 // zadanue 2
-void main() {
-  List<String> input = File("numsTask1.txt").readAsLinesSync();
-  for (var h in input) {
-    input = h.split(' ');
-    for (int i = 0; i < input.length; i++) {
-      stdout.write('${input[i]} ');
-    }
-  }
-}
-
-//zadanie 3 
 // void main() {
-//   stdout.write("Введите число: ");
-//   int nums = int.parse(stdin.readLineSync()!);
-//   if (nums % 10 == 0 && nums % 2 == 0) {
-//     print("Является");
-//   } else {
-//     print("Не является");
-//   }
-// }
-
-// // zadanie 4
-//   void main() {
-//   int n = 1;
-//   int sum = 0;
-//   print("Введите число: a: ");
-//   int a = int.parse(stdin.readLineSync()!);
-//   while (n > 0) {
-//     print("Введите число n: ");
-//     n = int.parse(stdin.readLineSync()!);
-//     if (n % a == 0) {
-//       sum += n;
+//   List<String> input = File("numsTask2.txt").readAsLinesSync();
+//   for (var h in input) {
+//     input = h.split(' ');
+//     for (int i = 0; i < input.length; i++) {
+//       stdout.write('${input[i]} ');
 //     }
 //   }
-//   print(sum);
-//   }
-
-//zadanie 3 
-// void main() {
-//   stdout.write("Введите число: ");
-//   int nums = int.parse(stdin.readLineSync()!);
-//   if (nums % 10 == 0 && nums % 2 == 0) {
-//     print("Является");
-//   } else {
-//     print("Не является");
-//   }
 // }
-
-// // zadanie 4
-//   void main() {
-//   int n = 1;
-//   int sum = 0;
-//   print("Введите число: a: ");
-//   int a = int.parse(stdin.readLineSync()!);
-//   while (n > 0) {
-//     print("Введите число n: ");
-//     n = int.parse(stdin.readLineSync()!);
-//     if (n % a == 0) {
-//       sum += n;
-//     }
-//   }
-//   print(sum);
-//   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //zadanie 3 
 // void main() {
